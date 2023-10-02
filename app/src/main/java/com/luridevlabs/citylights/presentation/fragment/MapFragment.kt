a1d197b1c7317ab7a5a5cd3cd22b5bb221ef8e02
laura <69944724+laurazp@users.noreply.github.com>
2023-10-02 13:33:00 +0200

    Location permissions managed

--- app/src/main/java/com/luridevlabs/citylights/presentation/fragment/MapFragment.kt
+++ app/src/main/java/com/luridevlabs/citylights/presentation/fragment/MapFragment.kt
@@ -1,22 +1,25 @@
 package com.luridevlabs.citylights.presentation.fragment
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Point
-import android.os.Build
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.luridevlabs.citylights.R
@@ -32,15 +35,20 @@ class MapFragment: Fragment(), OnMapReadyCallback {
     private lateinit var binding: FragmentMapBinding
     private val monumentsViewModel: MonumentsViewModel by activityViewModel()
     private var markersList: MutableList<MarkerOptions> = mutableListOf()
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val permissionId = 2
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMapBinding.inflate(layoutInflater)
-        return binding.root
 
-        //requestLocationPermissions()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getLocation()
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -80,6 +88,7 @@ class MapFragment: Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
 
         markersList.forEach { marker ->
             googleMap.addMarker(marker)
@@ -151,5 +160,88 @@ class MapFragment: Fragment(), OnMapReadyCallback {
                 //TODO: volver a cargar markers
                 //monumentsViewModel.fetchMonuments()
             }
+    }
+
+    private fun getLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                if (ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return
+                }
+                fusedLocationClient.lastLocation.addOnCompleteListener(requireActivity()) { task ->
+                    val location: Location? = task.result
+                    if (location != null) {
+                        //val geocoder = Geocoder(requireContext(), Locale.getDefault())
+
+                        //val list: MutableList<Address>? =
+                        //    geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                        //TODO: ¿?
+                        val zaragozaLatLng = LatLng(location.latitude, location.longitude)
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(zaragozaLatLng))
+                        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
+                    }
+                }
+            } else {
+                Toast.makeText(requireContext(), "Please turn on location", Toast.LENGTH_LONG).show()
+                //TODO: ¿?
+                requestPermissions()
+                //val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                //startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            permissionId
+        )
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == permissionId) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLocation()
+            }
+        }
     }
 }
