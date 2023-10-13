e65749ac57d46bc49a2f25a64c490faf6d567b5e
laura <69944724+laurazp@users.noreply.github.com>
2023-10-14 00:19:17 +0200

    ProgressBar added in MonumentListFragment

--- app/src/main/java/com/luridevlabs/citylights/presentation/fragment/MonumentListFragment.kt
+++ app/src/main/java/com/luridevlabs/citylights/presentation/fragment/MonumentListFragment.kt
@@ -9,14 +9,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.luridevlabs.citylights.databinding.FragmentMonumentListBinding
+import com.luridevlabs.citylights.presentation.common.ResourceState
+import com.luridevlabs.citylights.presentation.composables.CircularProgressBar
 import com.luridevlabs.citylights.presentation.composables.Navigation
-
-/**
- * Como la navegación y las clases más globales están realizadas en vista clásica
- * pero quería implementar también algunas pantallas mediante funciones Composables,
- * he incluido este Fragment como contenedor para la ComposeView donde los implementaré.
- */
+import com.luridevlabs.citylights.presentation.viewmodel.MonumentListState
+import com.luridevlabs.citylights.presentation.viewmodel.MonumentsViewModel
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
 /**
  * Como la navegación y las clases más globales están realizadas en vista clásica
@@ -26,6 +26,7 @@ import com.luridevlabs.citylights.presentation.composables.Navigation
 class MonumentListFragment : Fragment() {
 
     private lateinit var binding: FragmentMonumentListBinding
+    private val monumentsViewModel: MonumentsViewModel by activityViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,11 +39,11 @@ class MonumentListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //initContent()
-        initComposeUI()
+        initContent()
+        //initComposeUI()
     }
 
-    /*private fun initContent() {
+    private fun initContent() {
         monumentsViewModel.getMonumentListLiveData().observe(viewLifecycleOwner) { state ->
             if (state != null) handleMonumentListState(state)
         }
@@ -50,26 +51,34 @@ class MonumentListFragment : Fragment() {
         if (monumentsViewModel.getMonumentListLiveData().value == null) {
             monumentsViewModel.fetchMonuments()
         }
-    }*/
+    }
 
-    /*private fun handleMonumentListState(state: MonumentListState) {
+    private fun handleMonumentListState(state: MonumentListState) {
         when(state) {
             is ResourceState.Loading -> {
-                binding.pbMonumentList.visibility = View.VISIBLE
+                //binding.pbMonumentList.visibility = View.VISIBLE
+                binding.cvListComposeView.setContent {
+                    MaterialTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            CircularProgressBar()
+                        }
+                    }
+                }
             }
             is ResourceState.Success -> {
-                binding.pbMonumentList.visibility = View.GONE
-                monumentListAdapter.submitList(state.result)
+                //binding.pbMonumentList.visibility = View.GONE
+                initComposeUI()
+                //monumentListAdapter.submitList(state.result)
             }
             is ResourceState.Error -> {
-                binding.pbMonumentList.visibility = View.GONE
+                //binding.pbMonumentList.visibility = View.GONE
                 showErrorDialog(state.error)
             }
         }
-    }*/
-
-        //initComposeUI()
-
+    }
 
     private fun initComposeUI() {
         binding.cvListComposeView.setContent {
@@ -84,13 +93,13 @@ class MonumentListFragment : Fragment() {
         }
     }
 
-    /*private fun showErrorDialog(error: String) {
+    private fun showErrorDialog(error: String) {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle("Error")
             .setMessage(error)
             .setPositiveButton("Aceptar", null)
             .setNegativeButton("Reintentar") { dialog, witch ->
                 monumentsViewModel.fetchMonuments()
-            }*/
-
+            }
+    }
 }
